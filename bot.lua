a832248637e0b87068b743ac32311a7f88b8c56d
delaram16 <mmkmmk264@gmail.com>
2016-06-28 00:02:46 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = ""
+local bot_api_key = "233072224:AAFus_WXBgeaEJ4NrePBpy0WXjBpmg6ZHhs"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[😉 سلام 😉
